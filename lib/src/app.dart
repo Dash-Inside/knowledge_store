a833c75d4d61bd4a8f13f41a89891a654da36107
pocket-red-fox <pocket.red.fox@gmail.com>
2023-07-31 22:31:04 +0300

    feat: add knowledge app

---
+++ lib/src/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class KnowledgeApp extends StatelessWidget {
+  const KnowledgeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
